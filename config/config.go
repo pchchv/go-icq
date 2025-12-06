93f7180a6ce18e47bd192f27de7a8113885d297c
pchchv <ipchchv@gmail.com>
2025-12-06 05:44:54 +0300

    feat: add Config type

--- config/config.go
+++ config/config.go
@@ -1,1 +1,13 @@
 package config
+
+type Config struct {
+	BOSListeners            []string
+	BOSAdvertisedHostsPlain []string
+	BOSAdvertisedHostsSSL   []string
+	KerberosListeners       []string
+	TOCListeners            []string
+	DisableAuth             bool
+	APIListener             string
+	DBPath                  string
+	LogLevel                string
+}

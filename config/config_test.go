d6c7649937bed77352c29f8c84a6a5d2a6299f2c
pchchv <ipchchv@gmail.com>
2025-12-07 14:28:55 +0300

    test: implement contains function

--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,32 @@
 package config
+
+// contains it's a helper function to check if a string contains a substring.
+func contains(s, substr string) bool {
+	if len(substr) == 0 {
+		return true
+	}
+
+	if len(s) < len(substr) {
+		return false
+	}
+
+	if s == substr {
+		return true
+	}
+
+	if s[:len(substr)] == substr {
+		return true
+	}
+
+	if s[len(s)-len(substr):] == substr {
+		return true
+	}
+
+	for i := 1; i <= len(s)-len(substr); i++ {
+		if s[i:i+len(substr)] == substr {
+			return true
+		}
+	}
+
+	return false
+}

08ae7192f0d2f83b2f1947437a1022241b44c00f
pchchv <ipchchv@gmail.com>
2025-12-08 21:15:04 +0300

    test: implemenr Write method for errWriter

--- wire/encode_test.go
+++ wire/encode_test.go
@@ -1,4 +1,10 @@
 package wire
 
+import "io"
+
 // errWriter is a writer that always returns an error.
 type errWriter struct{}
+
+func (errWriter) Write(p []byte) (n int, err error) {
+	return 0, io.EOF
+}

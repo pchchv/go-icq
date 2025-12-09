33c4e3dd42b2a99d8f146d1a98f127e2f7b07803
pchchv <ipchchv@gmail.com>
2025-12-09 19:59:09 +0300

    feat: add ICQMessageRequestEnvelope type

--- wire/snacs.go
+++ wire/snacs.go
@@ -5,3 +5,9 @@ package wire
 type ICQMessageReplyEnvelope struct {
 	Message any `oscar:"len_prefix=uint16"`
 }
+
+// ICQMessageRequestEnvelope is a helper struct that provides syntactic sugar for
+// unmarshaling an ICQ message into a little-endian byte array.
+type ICQMessageRequestEnvelope struct {
+	Body []byte `oscar:"len_prefix=uint16"`
+}

66507f7ce8a139bcde29a0c66f0e698bb9273538
pchchv <ipchchv@gmail.com>
2025-12-09 19:21:00 +0300

    test(TLVList): add tests for methods Append, AppendList and Replace

--- wire/tlv_test.go
+++ wire/tlv_test.go
@@ -21,3 +21,102 @@ func TestTLVList_NewTLVLEPanic(t *testing.T) {
 		NewTLVLE(1, 30)
 	})
 }
+
+func TestTLVList_Append(t *testing.T) {
+	want := TLVList{
+		{
+			Tag:   0,
+			Value: []byte(`0`),
+		},
+		{
+			Tag:   1,
+			Value: []byte(`1`),
+		},
+		{
+			Tag:   2,
+			Value: []byte(`2`),
+		},
+	}
+
+	have := TLVList{}
+	have.Append(NewTLVBE(0, []byte(`0`)))
+	have.Append(NewTLVBE(1, []byte(`1`)))
+	have.Append(NewTLVBE(2, []byte(`2`)))
+
+	assert.Equal(t, want, have)
+}
+
+func TestTLVList_AppendList(t *testing.T) {
+	want := TLVList{
+		{
+			Tag:   0,
+			Value: []byte(`0`),
+		},
+		{
+			Tag:   1,
+			Value: []byte(`1`),
+		},
+		{
+			Tag:   2,
+			Value: []byte(`2`),
+		},
+	}
+
+	have := TLVList{}
+	have.AppendList([]TLV{
+		NewTLVBE(0, []byte(`0`)),
+		NewTLVBE(1, []byte(`1`)),
+		NewTLVBE(2, []byte(`2`)),
+	})
+
+	assert.Equal(t, want, have)
+}
+
+func TestTLVList_Replace(t *testing.T) {
+	tests := []struct {
+		name        string
+		given       TLVList
+		want        TLVList
+		replacement TLV
+	}{
+		{
+			name: "replace multiple TLVs",
+			given: TLVList{
+				NewTLVLE(0x01, []byte{0x01}),
+				NewTLVLE(0x02, []byte{0x02}),
+				NewTLVLE(0x01, []byte{0x03}),
+				NewTLVLE(0x03, []byte{0x04}),
+			},
+			replacement: NewTLVLE(0x01, []byte{0xAA}),
+			want: TLVList{
+				NewTLVLE(0x01, []byte{0xAA}),
+				NewTLVLE(0x02, []byte{0x02}),
+				NewTLVLE(0x01, []byte{0xAA}),
+				NewTLVLE(0x03, []byte{0x04}),
+			},
+		},
+		{
+			name: "no matching tags",
+			given: TLVList{
+				NewTLVLE(0x01, []byte{0x01}),
+				NewTLVLE(0x02, []byte{0x02}),
+				NewTLVLE(0x01, []byte{0x03}),
+				NewTLVLE(0x03, []byte{0x04}),
+			},
+			replacement: NewTLVLE(0x07, []byte{0xAA}),
+			want: TLVList{
+				NewTLVLE(0x01, []byte{0x01}),
+				NewTLVLE(0x02, []byte{0x02}),
+				NewTLVLE(0x01, []byte{0x03}),
+				NewTLVLE(0x03, []byte{0x04}),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.given.Replace(tt.replacement)
+			assert.Equal(t, tt.want, tt.given)
+		})
+	}
+}

803c5728a902794acc395a8a16f68c338e0af285
pchchv <ipchchv@gmail.com>
2025-12-10 20:52:26 +0300

    test: add tests for subGroup

--- wire/snacs_string_test.go
+++ wire/snacs_string_test.go
@@ -13,3 +13,11 @@ func TestFoodGroupName_HappyPath(t *testing.T) {
 func TestFoodGroupName_InvalidFoodGroup(t *testing.T) {
 	assert.Equal(t, "unknown", FoodGroupName(2142))
 }
+
+func TestSubGroupName_HappyPath(t *testing.T) {
+	assert.Equal(t, "OServiceServiceRequest", SubGroupName(OService, OServiceServiceRequest))
+}
+
+func TestSubGroupName_InvalidFoodGroup(t *testing.T) {
+	assert.Equal(t, "unknown", SubGroupName(2142, OServiceServiceRequest))
+}

ab05a8c14f5cdeab7e9df7d09dda4abd969a7192
pchchv <ipchchv@gmail.com>
2025-12-13 22:09:50 +0300

    feat: implement method ValidateAIMHandle for DisplayScreenName

--- state/user.go
+++ state/user.go
@@ -7,7 +7,11 @@ import (
 	"unicode"
 )
 
-var ErrICQUINInvalidFormat = errors.New("uin must be a number in the range 10000-2147483646")
+var (
+	ErrAIMHandleLength        = errors.New("screen name must be between 3 and 16 characters")
+	ErrICQUINInvalidFormat    = errors.New("uin must be a number in the range 10000-2147483646")
+	ErrAIMHandleInvalidFormat = errors.New("screen name must start with a letter, cannot end with a space, and must contain only letters, numbers, and spaces")
+)
 
 // IdentScreenName struct stores the normalized version of a user's screen name.
 // This format is used for uniformity in storage and comparison by removing spaces
@@ -79,5 +83,38 @@ func (s DisplayScreenName) ValidateUIN() error {
 	if err != nil || uin < 10000 || uin > 2147483646 {
 		return ErrICQUINInvalidFormat
 	}
+	return nil
+}
+
+// ValidateAIMHandle returns an error if the instance is not a valid AIM screen name.
+// Possible errors:
+//   - ErrAIMHandleLength: if the screen name has less than 3 non-space
+//     characters or more than 16 characters (including spaces).
+//   - ErrAIMHandleInvalidFormat: if the screen name does not start with a letter,
+//     ends with a space, or contains invalid characters.
+func (s DisplayScreenName) ValidateAIMHandle() error {
+	if len(s) > 16 {
+		return ErrAIMHandleLength
+	}
+
+	var c int
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			c++
+		} else if r != ' ' {
+			return ErrAIMHandleInvalidFormat
+		}
+	}
+
+	if c < 3 {
+		return ErrAIMHandleLength
+	}
+
+	// Must start with a letter, cannot end with a space,
+	// and must contain only letters, numbers, and spaces.
+	if !unicode.IsLetter(rune(s[0])) || s[len(s)-1] == ' ' {
+		return ErrAIMHandleInvalidFormat
+	}
+
 	return nil
 }

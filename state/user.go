e6f1146a06b53b851ab527ee00e6e2be92c70372
pchchv <ipchchv@gmail.com>
2025-12-13 22:06:30 +0300

    feat: implement IsUIN and ValidateUIN methods for DisplayScreenName

--- state/user.go
+++ state/user.go
@@ -1,9 +1,13 @@
 package state
 
 import (
+	"errors"
 	"strconv"
 	"strings"
+	"unicode"
 )
+
+var ErrICQUINInvalidFormat = errors.New("uin must be a number in the range 10000-2147483646")
 
 // IdentScreenName struct stores the normalized version of a user's screen name.
 // This format is used for uniformity in storage and comparison by removing spaces
@@ -50,4 +54,30 @@ func (s DisplayScreenName) IdentScreenName() IdentScreenName {
 // preserving the user-defined casing and spaces.
 func (s DisplayScreenName) String() string {
 	return string(s)
+}
+
+// IsUIN indicates whether the screen name is an ICQ UIN.
+func (s DisplayScreenName) IsUIN() bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// ValidateUIN returns an error if the instance is not a valid ICQ UIN.
+// Possible error is an ErrICQUINInvalidFormat
+// (if the UIN is not a number or is not in the valid range).
+func (s DisplayScreenName) ValidateUIN() error {
+	uin, err := strconv.Atoi(string(s))
+	if err != nil || uin < 10000 || uin > 2147483646 {
+		return ErrICQUINInvalidFormat
+	}
+	return nil
 }

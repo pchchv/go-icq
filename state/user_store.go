16ceec4d6fe615c542e7c07f92a660e51e8fd32d
pchchv <ipchchv@gmail.com>
2025-12-15 09:19:27 +0300

    feat: implement runMigrations method for SQLiteUserStore

--- state/user_store.go
+++ state/user_store.go
@@ -1,9 +1,51 @@
 package state
 
-import "database/sql"
+import (
+	"database/sql"
+	"embed"
+	"errors"
+	"fmt"
+	"io/fs"
+	"net/http"
+
+	"github.com/golang-migrate/migrate/v4"
+	migratesqlite "github.com/golang-migrate/migrate/v4/database/sqlite"
+	"github.com/golang-migrate/migrate/v4/source/httpfs"
+)
+
+//go:embed migrations/*
+var migrations embed.FS
 
 // SQLiteUserStore stores user feedbag (buddy list), profile,
 // and authentication credentials information in a SQLite database.
 type SQLiteUserStore struct {
 	db *sql.DB
+}
+
+func (u SQLiteUserStore) runMigrations() error {
+	migrationFS, err := fs.Sub(migrations, "migrations")
+	if err != nil {
+		return fmt.Errorf("failed to prepare migration subdirectory: %v", err)
+	}
+
+	sourceInstance, err := httpfs.New(http.FS(migrationFS), ".")
+	if err != nil {
+		return fmt.Errorf("failed to create source instance from embedded filesystem: %v", err)
+	}
+
+	driver, err := migratesqlite.WithInstance(u.db, &migratesqlite.Config{})
+	if err != nil {
+		return fmt.Errorf("cannot create database driver: %v", err)
+	}
+
+	m, err := migrate.NewWithInstance("httpfs", sourceInstance, "sqlite", driver)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %v", err)
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("failed to run migrations: %v", err)
+	}
+
+	return nil
 }

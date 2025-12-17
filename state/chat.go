a284875f1c2323ca8e078e06676f4a77c8870f3d
pchchv <ipchchv@gmail.com>
2025-12-17 20:08:55 +0300

    feat: implement ChatRoom type with constructor

---
+++ state/chat.go
@@ -0,0 +1,20 @@
+package state
+
+import "time"
+
+// ChatRoom represents of a chat room.
+type ChatRoom struct {
+	name       string
+	creator    IdentScreenName
+	exchange   uint16
+	createTime time.Time
+}
+
+// NewChatRoom creates a new ChatRoom instance.
+func NewChatRoom(name string, creator IdentScreenName, exchange uint16) ChatRoom {
+	return ChatRoom{
+		name:     name,
+		creator:  creator,
+		exchange: exchange,
+	}
+}

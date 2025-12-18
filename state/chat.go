2ef4dfeea08eed98e3d9c3daec7f079539484b50
pchchv <ipchchv@gmail.com>
2025-12-18 19:40:31 +0300

    feat: implement Creator and InstanceNumber methods for ChatRoom

--- state/chat.go
+++ state/chat.go
@@ -26,3 +26,14 @@ func NewChatRoom(name string, creator IdentScreenName, exchange uint16) ChatRoom
 		exchange: exchange,
 	}
 }
+
+// Creator returns the screen name of the user who created the chat room.
+func (c ChatRoom) Creator() IdentScreenName {
+	return c.creator
+}
+
+// InstanceNumber returns which instance chatroom exists in. Overflow chat
+// rooms do not exist yet, so all chats happen in the same instance.
+func (c ChatRoom) InstanceNumber() uint16 {
+	return 0
+}

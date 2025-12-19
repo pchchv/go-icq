856c78fa8235eaa39949c733c77cac3c6d052a98
pchchv <ipchchv@gmail.com>
2025-12-19 22:45:42 +0300

    test(InMemorySessionManager): add tests for Retrieve and RetrieveSession methods

--- state/session_manager_test.go
+++ state/session_manager_test.go
@@ -44,3 +44,70 @@ func TestInMemorySessionManager_AddSession_SessionConflict(t *testing.T) {
 	assert.Nil(t, sess2)
 	assert.ErrorIs(t, err, errSessConflict)
 }
+
+func TestInMemorySessionManager_Retrieve(t *testing.T) {
+	tests := []struct {
+		name             string
+		given            []DisplayScreenName
+		lookupScreenName IdentScreenName
+		wantScreenName   IdentScreenName
+	}{
+		{
+			name: "lookup finds match",
+			given: []DisplayScreenName{
+				"user-screen-name-1",
+				"user-screen-name-2",
+			},
+			lookupScreenName: NewIdentScreenName("user-screen-name-2"),
+			wantScreenName:   NewIdentScreenName("user-screen-name-2"),
+		},
+		{
+			name:             "lookup does not find match",
+			given:            []DisplayScreenName{},
+			lookupScreenName: NewIdentScreenName("user-screen-name-3"),
+			wantScreenName:   NewIdentScreenName(""),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sm := NewInMemorySessionManager(slog.Default())
+			for _, screenName := range tt.given {
+				sess, err := sm.AddSession(context.Background(), screenName)
+				assert.NoError(t, err)
+				sess.SetSignonComplete()
+			}
+
+			if have := sm.RetrieveSession(tt.lookupScreenName); have == nil {
+				assert.Empty(t, tt.wantScreenName)
+			} else {
+				assert.Equal(t, tt.wantScreenName, have.IdentScreenName())
+			}
+		})
+	}
+}
+
+func TestInMemorySessionManager_RetrieveSession_IncompleteSignon(t *testing.T) {
+	sm := NewInMemorySessionManager(slog.Default())
+	user1, err := sm.AddSession(context.Background(), "user-screen-name-1")
+	assert.NoError(t, err)
+	// user1 has not completed signon
+
+	sess := sm.RetrieveSession(NewIdentScreenName("user-screen-name-1"))
+	assert.Nil(t, sess, "should return nil for session with incomplete signon")
+
+	user1.SetSignonComplete()
+	sess = sm.RetrieveSession(NewIdentScreenName("user-screen-name-1"))
+	assert.NotNil(t, sess, "should return session after signon is complete")
+	assert.Equal(t, user1, sess)
+}
+
+func TestInMemorySessionManager_RetrieveSession_CompleteSignon(t *testing.T) {
+	sm := NewInMemorySessionManager(slog.Default())
+	user1, err := sm.AddSession(context.Background(), "user-screen-name-1")
+	assert.NoError(t, err)
+	user1.SetSignonComplete()
+
+	sess := sm.RetrieveSession(NewIdentScreenName("user-screen-name-1"))
+	assert.NotNil(t, sess)
+	assert.Equal(t, user1, sess)
+}

10eed02daf57bb31b76ab4cdb89e878edc841a73
pchchv <ipchchv@gmail.com>
2026-01-15 10:35:33 +0300

    feat: add an UserInfo type

--- server/webapi/types/event_types.go
+++ server/webapi/types/event_types.go
@@ -13,5 +13,14 @@ const (
 	EventTypeRateLimit    EventType = "rateLimit"
 )
 
+// UserInfo represents basic user information in events.
+type UserInfo struct {
+	AimID      string  `json:"aimId"`
+	DisplayID  string  `json:"displayId,omitempty"`
+	UserType   string  `json:"userType,omitempty"`
+	State      string  `json:"state,omitempty"`
+	OnlineTime float64 `json:"onlineTime,omitempty"` // float64 for AMF3 encoding
+}
+
 // EventType defines the type of WebAPI event.
 type EventType string

a494cd123a8e4c8b71aef6df2c56892408b44338
pchchv <ipchchv@gmail.com>
2026-01-16 09:50:13 +0300

    feat: add ChatEventType and ChatEventData types

--- state/webapi_chat.go
+++ state/webapi_chat.go
@@ -1,9 +1,27 @@
 package state
 
-const ChatRoomTypeUserCreated ChatRoomType = "userCreated"
+const (
+	ChatRoomTypeUserCreated ChatRoomType  = "userCreated"
+	ChatEventUserEntered    ChatEventType = "userEntered"
+	ChatEventUserInRoom     ChatEventType = "userInRoom"
+	ChatEventUserLeft       ChatEventType = "userLeft"
+	ChatEventMessage        ChatEventType = "message"
+	ChatEventTyping         ChatEventType = "typing"
+	ChatEventClosed         ChatEventType = "closed"
+)
 
 // ChatRoomType represents the type of chat room.
 type ChatRoomType string
+
+// ChatEventType represents the type of chat event.
+type ChatEventType string
+
+// ChatEventData represents data for a chat event.
+type ChatEventData struct {
+	ChatSID   string        `json:"chatsid"`
+	EventType ChatEventType `json:"eventType"`
+	EventData interface{}   `json:"eventData"`
+}
 
 // ChatSession represents a user's session in a chat room.
 type ChatSession struct {

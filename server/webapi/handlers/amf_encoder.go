e4c391b817c64f7553f2ece28b0d27afcf46e032
pchchv <ipchchv@gmail.com>
2026-01-19 10:19:34 +0300

    feat: add AMFEncoder type with constructor

--- server/webapi/handlers/amf_encoder.go
+++ server/webapi/handlers/amf_encoder.go
@@ -1,6 +1,18 @@
 package handlers
 
+import "log/slog"
+
 const AMF3 AMFVersion = 3
 
 // AMFVersion represents the AMF encoding version.
 type AMFVersion int
+
+// AMFEncoder handles AMF encoding operations for WebAPI responses.
+type AMFEncoder struct {
+	logger *slog.Logger
+}
+
+// NewAMFEncoder creates a new AMF encoder instance.
+func NewAMFEncoder(logger *slog.Logger) *AMFEncoder {
+	return &AMFEncoder{logger: logger}
+}

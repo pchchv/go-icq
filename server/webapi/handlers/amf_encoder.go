1900a43f8891cb141e0a1ce9f8ecdb2f3ce9744c
pchchv <ipchchv@gmail.com>
2026-01-19 10:27:03 +0300

    feat(AMFEncoder): implement isZeroValue and toAMF3Compatible methods

--- server/webapi/handlers/amf_encoder.go
+++ server/webapi/handlers/amf_encoder.go
@@ -1,6 +1,10 @@
 package handlers
 
-import "log/slog"
+import (
+	"log/slog"
+	"reflect"
+	"time"
+)
 
 const AMF3 AMFVersion = 3
 
@@ -15,4 +19,36 @@ type AMFEncoder struct {
 // NewAMFEncoder creates a new AMF encoder instance.
 func NewAMFEncoder(logger *slog.Logger) *AMFEncoder {
 	return &AMFEncoder{logger: logger}
+}
+
+// isZeroValue checks if a reflect.Value is a zero value.
+func (e *AMFEncoder) isZeroValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	case reflect.Struct:
+		// for time.Time, check if it's zero
+		if t, ok := v.Interface().(time.Time); ok {
+			return t.IsZero()
+		}
+		// for other structs, we can't easily determine zero value
+		return false
+	default:
+		return false
+	}
+}
+
+// toAMF3Compatible converts Go types to AMF3-compatible format for goAMF3.
+func (e *AMFEncoder) toAMF3Compatible(data interface{}) interface{} {
+	return map[string]interface{}{}
 }

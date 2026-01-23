1b2a2ac0ed5b5fba7916612b971ebb734b812c24
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-23 14:37:49 +0000

    feat(BuddyListManager): implement FormatBuddyListEvent method

--- server/webapi/handlers/buddy_list_manager.go
+++ server/webapi/handlers/buddy_list_manager.go
@@ -1,5 +1,6 @@
 package handlers
 
+import "log/slog"
 
 // WebAPIBuddyInfo represents a buddy in the WebAPI format.
 type WebAPIBuddyInfo struct {
@@ -41,5 +42,81 @@ func NewBuddyListManager(feedbagRetriever FeedbagRetriever, sessionRetriever Ses
 		feedbagRetriever: feedbagRetriever,
 		sessionRetriever: sessionRetriever,
 		logger:           logger,
+	}
+}
+
+// FormatBuddyListEvent formats a buddy list for an event.
+func (m *BuddyListManager) FormatBuddyListEvent(groups []WebAPIBuddyGroup) map[string]interface{} {
+	// convert groups to a format that AMF3 can properly encode AMF3 has trouble with complex struct slices, convert to maps
+	groupMaps := make([]interface{}, len(groups))
+	for i, group := range groups {
+		buddyMaps := make([]interface{}, len(group.Buddies))
+		for j, buddy := range group.Buddies {
+			// convert each buddy to a map
+			buddyMap := map[string]interface{}{
+				"aimId":     buddy.AimID,
+				"displayId": buddy.DisplayID,
+				"state":     buddy.State,
+				"userType":  buddy.UserType,
+				"bot":       buddy.Bot,
+				"service":   buddy.Service,
+			}
+
+			// add optional fields if present
+			if buddy.StatusMsg != "" {
+				buddyMap["statusMsg"] = buddy.StatusMsg
+			}
+
+			if buddy.AwayMsg != "" {
+				buddyMap["awayMsg"] = buddy.AwayMsg
+			}
+
+			if buddy.OnlineTime > 0 {
+				buddyMap["onlineTime"] = float64(buddy.OnlineTime)
+			}
+
+			if buddy.IdleTime > 0 {
+				buddyMap["idleTime"] = buddy.IdleTime
+			}
+
+			if buddy.PresenceIcon != "" {
+				buddyMap["presenceIcon"] = buddy.PresenceIcon
+			}
+
+			if buddy.BuddyIcon != "" {
+				buddyMap["buddyIcon"] = buddy.BuddyIcon
+			}
+
+			if len(buddy.Capabilities) > 0 {
+				buddyMap["capabilities"] = buddy.Capabilities
+			}
+
+			if buddy.MemberSince > 0 {
+				buddyMap["memberSince"] = float64(buddy.MemberSince)
+			}
+
+			buddyMaps[j] = buddyMap
+		}
+
+		// convert group to a map
+		groupMap := map[string]interface{}{
+			"name":    group.Name,
+			"buddies": buddyMaps,
+		}
+
+		// add optional group fields
+		if group.Recent {
+			groupMap["recent"] = group.Recent
+		}
+
+		if group.Smart != nil {
+			groupMap["smart"] = group.Smart
+		}
+
+		groupMaps[i] = groupMap
+	}
+
+	return map[string]interface{}{
+		"groups": groupMaps,
 	}
 }

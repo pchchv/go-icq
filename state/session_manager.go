f167bc419d504f67254a53ef5043d8b007e10158
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-25 08:39:55 +0000

    retfactor: update InMemorySessionManager type

--- state/session_manager.go
+++ state/session_manager.go
@@ -28,16 +28,21 @@ type userLock struct {
 // provides synchronized message relay between sessions in the session pool.
 // An InMemorySessionManager is safe for concurrent use by multiple goroutines.
 type InMemorySessionManager struct {
-	store    map[IdentScreenName]*sessionSlot
-	mapMutex sync.RWMutex
-	logger   *slog.Logger
+	store                 map[IdentScreenName]*sessionSlot
+	logger                *slog.Logger
+	mapMutex              sync.RWMutex
+	userLocks             map[IdentScreenName]*userLock
+	userLocksMutex        sync.Mutex
+	maxConcurrentSessions int
 }
 
 // NewInMemorySessionManager creates a new instance of InMemorySessionManager.
 func NewInMemorySessionManager(logger *slog.Logger) *InMemorySessionManager {
 	return &InMemorySessionManager{
-		logger: logger,
-		store:  make(map[IdentScreenName]*sessionSlot),
+		store:                 make(map[IdentScreenName]*sessionSlot),
+		logger:                logger,
+		userLocks:             make(map[IdentScreenName]*userLock),
+		maxConcurrentSessions: DefaultMaxConcurrentSessions,
 	}
 }
 

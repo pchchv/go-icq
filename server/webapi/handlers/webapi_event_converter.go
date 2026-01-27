4245cd74ec9350a680d7d196c042f0eae918c74b
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-27 12:35:27 +0000

    feat: implement convertTimestampsInMap function

--- server/webapi/handlers/webapi_event_converter.go
+++ server/webapi/handlers/webapi_event_converter.go
@@ -22,3 +22,35 @@ func isTimestampField(fieldName string) bool {
 
 	return false
 }
+
+// convertTimestampsInMap recursively converts int64 values that look like timestamps to float64.
+func convertTimestampsInMap(data map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for key, val := range data {
+		// check if key suggests it's a timestamp
+		if isTimestampField(key) {
+			if intVal, ok := val.(int64); ok {
+				result[key] = float64(intVal)
+				continue
+			}
+		}
+
+		// recursively process nested maps
+		if nestedMap, ok := val.(map[string]interface{}); ok {
+			result[key] = convertTimestampsInMap(nestedMap)
+		} else if nestedSlice, ok := val.([]interface{}); ok {
+			convertedSlice := make([]interface{}, len(nestedSlice))
+			for i, item := range nestedSlice {
+				if itemMap, ok := item.(map[string]interface{}); ok {
+					convertedSlice[i] = convertTimestampsInMap(itemMap)
+				} else {
+					convertedSlice[i] = item
+				}
+			}
+			result[key] = convertedSlice
+		} else {
+			result[key] = val
+		}
+	}
+	return result
+}

5d67f64659b75eb8e2ee9c79812f05a560b203eb
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-28 06:53:52 +0000

    feat: add OfflineMessageManager type

--- server/webapi/handlers/messaging.go
+++ server/webapi/handlers/messaging.go
@@ -6,7 +6,12 @@ import (
 	"github.com/pchchv/go-icq/state"
 )
 
-// RelationshipFetcher defines methods for fetching user relationships
+// RelationshipFetcher defines methods for fetching user relationships.
 type RelationshipFetcher interface {
 	Relationship(ctx context.Context, me state.IdentScreenName, them state.IdentScreenName) (state.Relationship, error)
+}
+
+// OfflineMessageManager defines methods for managing offline messages.
+type OfflineMessageManager interface {
+	SaveMessage(ctx context.Context, msg state.OfflineMessage) (int, error)
 }

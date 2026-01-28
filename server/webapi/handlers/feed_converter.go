133ef77c59a2b5adc484fc5290ce267125df9b20
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-28 07:16:32 +0000

    feat: add an AtomContent type

--- server/webapi/handlers/feed_converter.go
+++ server/webapi/handlers/feed_converter.go
@@ -8,3 +8,8 @@ type AtomLink struct {
 	Href string `xml:"href,attr"`
 	Rel  string `xml:"rel,attr,omitempty"`
 }
+
+type AtomContent struct {
+	Type string `xml:"type,attr"`
+	Text string `xml:",chardata"`
+}

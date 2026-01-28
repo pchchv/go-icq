c82ed1372f93d9819eaa3dd131a2655a86bbbac9
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-28 06:54:12 +0000

    feat: add MessageRelayer type

--- server/webapi/handlers/messaging.go
+++ server/webapi/handlers/messaging.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/pchchv/go-icq/state"
+	"github.com/pchchv/go-icq/wire"
 )
 
 // RelationshipFetcher defines methods for fetching user relationships.
@@ -14,4 +15,9 @@ type RelationshipFetcher interface {
 // OfflineMessageManager defines methods for managing offline messages.
 type OfflineMessageManager interface {
 	SaveMessage(ctx context.Context, msg state.OfflineMessage) (int, error)
+}
+
+// MessageRelayer defines methods for relaying messages between users.
+type MessageRelayer interface {
+	RelayToScreenName(ctx context.Context, recipient state.IdentScreenName, msg wire.SNACMessage)
 }

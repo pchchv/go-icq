861c39185999051ea773e101f356e7f3529fa43d
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-28 06:54:39 +0000

    feat: add MessagingHandler type

--- server/webapi/handlers/messaging.go
+++ server/webapi/handlers/messaging.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/pchchv/go-icq/state"
 	"github.com/pchchv/go-icq/wire"
@@ -20,4 +21,14 @@ type OfflineMessageManager interface {
 // MessageRelayer defines methods for relaying messages between users.
 type MessageRelayer interface {
 	RelayToScreenName(ctx context.Context, recipient state.IdentScreenName, msg wire.SNACMessage)
+}
+
+// MessagingHandler handles Web AIM API messaging endpoints.
+type MessagingHandler struct {
+	OfflineMessageManager OfflineMessageManager
+	RelationshipFetcher   RelationshipFetcher
+	SessionRetriever      SessionRetriever
+	SessionManager        *state.WebAPISessionManager
+	MessageRelayer        MessageRelayer
+	Logger                *slog.Logger
 }

44fce1e96a659995ce031294d57e0a90ce9a15bd
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-29 07:34:00 +0000

    feat(MockWebAPISessionManager): implement GetSession and TouchSession methods

--- server/webapi/handlers/buddylist_test.go
+++ server/webapi/handlers/buddylist_test.go
@@ -1,8 +1,26 @@
 package handlers
 
-import "github.com/stretchr/testify/mock"
+import (
+	"context"
+
+	"github.com/pchchv/go-icq/state"
+	"github.com/stretchr/testify/mock"
+)
 
 // MockWebAPISessionManager is a mock implementation of the WebAPISessionManager
 type MockWebAPISessionManager struct {
 	mock.Mock
+}
+
+func (m *MockWebAPISessionManager) GetSession(ctx context.Context, aimsid string) (*state.WebAPISession, error) {
+	args := m.Called(ctx, aimsid)
+	if session := args.Get(0); session != nil {
+		return session.(*state.WebAPISession), args.Error(1)
+	}
+	return nil, args.Error(1)
+}
+
+func (m *MockWebAPISessionManager) TouchSession(ctx context.Context, aimsid string) error {
+	args := m.Called(ctx, aimsid)
+	return args.Error(0)
 }

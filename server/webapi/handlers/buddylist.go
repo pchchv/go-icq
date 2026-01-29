40c209467f5445119893cc49f4884e3d1d3c5ae7
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-29 07:03:30 +0000

    feat: add FeedbagManager type

--- server/webapi/handlers/buddylist.go
+++ server/webapi/handlers/buddylist.go
@@ -4,10 +4,19 @@ import (
 	"context"
 
 	"github.com/pchchv/go-icq/state"
+	"github.com/pchchv/go-icq/wire"
 )
 
 // WebAPISessionManager provides methods to manage WebAPI sessions.
 type WebAPISessionManager interface {
 	GetSession(ctx context.Context, aimsid string) (*state.WebAPISession, error)
 	TouchSession(ctx context.Context, aimsid string) error
+}
+
+// FeedbagManager provides methods to manage buddy lists.
+type FeedbagManager interface {
+	RetrieveFeedbag(ctx context.Context, screenName state.IdentScreenName) ([]wire.FeedbagItem, error)
+	InsertItem(ctx context.Context, screenName state.IdentScreenName, item wire.FeedbagItem) error
+	UpdateItem(ctx context.Context, screenName state.IdentScreenName, item wire.FeedbagItem) error
+	DeleteItem(ctx context.Context, screenName state.IdentScreenName, item wire.FeedbagItem) error
 }

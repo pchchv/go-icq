4b7878e47704c14a45604b4ddbc2ad7f683981b3
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-30 07:00:39 +0000

    feat: add StartOSCARSessionResponse type

--- server/webapi/handlers/oscar_bridge.go
+++ server/webapi/handlers/oscar_bridge.go
@@ -1,5 +1,7 @@
 package handlers
 
+import "encoding/xml"
+
 // CookieBaker issues and validates authentication cookies for OSCAR services.
 type CookieBaker interface {
 	// Issue creates a new authentication cookie from the given payload.
@@ -13,4 +15,32 @@ type StartOSCARSessionRequest struct {
 	AimSID   string // WebAPI session ID
 	UseSSL   bool   // Whether to use SSL for the OSCAR connection
 	Compress bool   // Whether to use compression (not implemented)
+}
+
+// StartOSCARSessionResponse represents the response for startOSCARSession endpoint.
+type StartOSCARSessionResponse struct {
+	XMLName  xml.Name `xml:"response" json:"-"`
+	Response struct {
+		StatusCode int    `json:"statusCode" xml:"statusCode"`
+		StatusText string `json:"statusText" xml:"statusText"`
+		Data       struct {
+			Host        string `json:"host" xml:"host"`
+			Port        int    `json:"port" xml:"port"`
+			Cookie      string `json:"cookie" xml:"cookie"`
+			UseSSL      bool   `json:"useSSL" xml:"useSSL"`
+			Encryption  string `json:"encryption,omitempty" xml:"encryption,omitempty"`
+			Compression string `json:"compression,omitempty" xml:"compression,omitempty"`
+		} `json:"data" xml:"data"`
+	} `json:"response" xml:"-"`
+	// For XML responses, flatten the structure.
+	StatusCode int    `json:"-" xml:"statusCode"`
+	StatusText string `json:"-" xml:"statusText"`
+	Data       struct {
+		Host        string `json:"-" xml:"host"`
+		Port        int    `json:"-" xml:"port"`
+		Cookie      string `json:"-" xml:"cookie"`
+		UseSSL      bool   `json:"-" xml:"useSSL"`
+		Encryption  string `json:"-" xml:"encryption,omitempty"`
+		Compression string `json:"-" xml:"compression,omitempty"`
+	} `json:"-" xml:"data"`
 }

e7ccb73b163b938cca871da3b64625809d3b367d
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-30 06:35:30 +0000

    feat: add PreferenceManager type

--- server/webapi/handlers/preference.go
+++ server/webapi/handlers/preference.go
@@ -1,8 +1,20 @@
 package handlers
 
+import (
+	"context"
+
+	"github.com/pchchv/go-icq/state"
+)
+
 // PermitDenyData contains permit/deny list information.
 type PermitDenyData struct {
 	PDMode     int      `json:"pdMode" xml:"pdMode"`
-	PermitList []string `json:"permitList,omitempty" xml:"permitList>user,omitempty"`
 	DenyList   []string `json:"denyList,omitempty" xml:"denyList>user,omitempty"`
+	PermitList []string `json:"permitList,omitempty" xml:"permitList>user,omitempty"`
+}
+
+// PreferenceManager provides methods to manage user preferences.
+type PreferenceManager interface {
+	SetPreferences(ctx context.Context, screenName state.IdentScreenName, prefs map[string]interface{}) error
+	GetPreferences(ctx context.Context, screenName state.IdentScreenName) (map[string]interface{}, error)
 }

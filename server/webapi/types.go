447a6b93dc1e32840f2143c3e4e9f30f3eff4a26
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-31 08:03:34 +0000

    feat: add FeedbagManager type

--- server/webapi/types.go
+++ server/webapi/types.go
@@ -58,3 +58,11 @@ type BuddyListRegistry interface {
 	RegisterBuddyList(ctx context.Context, user state.IdentScreenName) error
 	UnregisterBuddyList(ctx context.Context, user state.IdentScreenName) error
 }
+
+// FeedbagManager provides methods to manage buddy lists.
+type FeedbagManager interface {
+	RetrieveFeedbag(ctx context.Context, screenName state.IdentScreenName) ([]wire.FeedbagItem, error)
+	InsertItem(ctx context.Context, screenName state.IdentScreenName, item wire.FeedbagItem) error
+	UpdateItem(ctx context.Context, screenName state.IdentScreenName, item wire.FeedbagItem) error
+	DeleteItem(ctx context.Context, screenName state.IdentScreenName, item wire.FeedbagItem) error
+}

8c110bc07a396d7019b06bbe406a3a4f3c6b8f5e
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-31 07:57:04 +0000

    feat: add OSCARBridgeStore type

--- server/webapi/types.go
+++ server/webapi/types.go
@@ -1,9 +1,23 @@
 package webapi
 
+import (
+	"context"
+
+	"github.com/pchchv/go-icq/state"
+)
+
 // OSCARConfig provides configuration for OSCAR services.
 type OSCARConfig interface {
 	GetSSLBOSAddress() (host string, port int)
 	GetBOSAddress() (host string, port int)
 	IsSSLAvailable() bool
 	IsAuthDisabled() bool
+}
+
+// OSCARBridgeStore manages the persistence of OSCAR bridge sessions.
+type OSCARBridgeStore interface {
+	SaveBridgeSession(ctx context.Context, webSessionID string, oscarCookie []byte, bosHost string, bosPort int) error
+	SaveBridgeSessionWithDetails(ctx context.Context, session *state.OSCARBridgeSession) error
+	GetBridgeSession(ctx context.Context, webSessionID string) (*state.OSCARBridgeSession, error)
+	DeleteBridgeSession(ctx context.Context, webSessionID string) error
 }

53283eb79866594b83f129d64f1e9ab7955862fe
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-01 09:24:18 +0000

    feat: add Server type

---
+++ server/webapi/server.go
@@ -0,0 +1,13 @@
+package webapi
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+// Server hosts an HTTP endpoint capable of handling AIM-style Kerberos authentication.
+// The messages are structured as SNACs transmitted over HTTP.
+type Server struct {
+	servers []*http.Server
+	logger  *slog.Logger
+}

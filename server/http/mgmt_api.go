a9deab6a7c752a8e17b3ce347583e8c7dbc8ec07
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-03 07:05:38 +0000

    feat(User): implement POST handler

--- server/http/mgmt_api.go
+++ server/http/mgmt_api.go
@@ -7,6 +7,9 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/pchchv/go-icq/state"
 )
 
 type Server struct {
@@ -26,6 +29,51 @@ func (s *Server) ListenAndServe() error {
 func (s *Server) Shutdown(ctx context.Context) error {
 	defer s.logger.Info("shutdown complete")
 	return s.server.Shutdown(ctx)
+}
+
+// postUserHandler handles the POST /user endpoint.
+func postUserHandler(w http.ResponseWriter, r *http.Request, userManager UserManager, newUUID func() uuid.UUID, logger *slog.Logger) {
+	input, err := userFromBody(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sn := state.DisplayScreenName(input.ScreenName)
+	if sn.IsUIN() {
+		if err := sn.ValidateUIN(); err != nil {
+			http.Error(w, fmt.Sprintf("invalid uin: %s", err), http.StatusBadRequest)
+			return
+		}
+	} else if err := sn.ValidateAIMHandle(); err != nil {
+		http.Error(w, fmt.Sprintf("invalid screen name: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	user := state.User{
+		AuthKey:           newUUID().String(),
+		DisplayScreenName: sn,
+		IdentScreenName:   sn.IdentScreenName(),
+		IsICQ:             sn.IsUIN(),
+	}
+	if err := user.HashPassword(input.Password); err != nil {
+		http.Error(w, fmt.Sprintf("invalid password: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	err = userManager.InsertUser(r.Context(), user)
+	switch {
+	case errors.Is(err, state.ErrDupUser):
+		http.Error(w, "user already exists", http.StatusConflict)
+		return
+	case err != nil:
+		logger.Error("error inserting user POST /user", "err", err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_, _ = fmt.Fprintln(w, "User account created successfully.")
 }
 
 func userFromBody(r *http.Request) (u userWithPassword, err error) {

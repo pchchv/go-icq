e08f1dc0668c6325af24d49cb1fb75fdd0bdc631
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-03 06:57:39 +0000

    feat(Server): implement ListenAndServe and Shutdown methods

--- server/http/mgmt_api.go
+++ server/http/mgmt_api.go
@@ -1,6 +1,9 @@
 package http
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 )
@@ -8,4 +11,18 @@ import (
 type Server struct {
 	server http.Server
 	logger *slog.Logger
+}
+
+func (s *Server) ListenAndServe() error {
+	s.logger.Info("starting server", "addr", s.server.Addr)
+	if err := s.server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("unable to start management API server: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	defer s.logger.Info("shutdown complete")
+	return s.server.Shutdown(ctx)
 }

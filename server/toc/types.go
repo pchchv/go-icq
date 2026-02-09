7c38ef4d3ef5b9458286cff1e11e797b8a318cd3
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-09 09:45:06 +0000

    feat: add an AuthService type

--- server/toc/types.go
+++ server/toc/types.go
@@ -3,6 +3,7 @@ package toc
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/pchchv/go-icq/state"
 	"github.com/pchchv/go-icq/wire"
 )
@@ -43,4 +44,16 @@ type BuddyService interface {
 
 type AdminService interface {
 	InfoChangeRequest(ctx context.Context, instance *state.SessionInstance, inFrame wire.SNACFrame, inBody wire.SNAC_0x07_0x04_AdminInfoChangeRequest) (wire.SNACMessage, error)
+}
+
+type AuthService interface {
+	BUCPChallenge(ctx context.Context, inBody wire.SNAC_0x17_0x06_BUCPChallengeRequest, newUUID func() uuid.UUID) (wire.SNACMessage, error)
+	BUCPLogin(ctx context.Context, inBody wire.SNAC_0x17_0x02_BUCPLoginRequest, advertisedHost string) (wire.SNACMessage, error)
+	CrackCookie(authCookie []byte) (state.ServerCookie, error)
+	FLAPLogin(ctx context.Context, inFrame wire.FLAPSignonFrame, advertisedHost string) (wire.TLVRestBlock, error)
+	RegisterBOSSession(ctx context.Context, authCookie state.ServerCookie) (*state.SessionInstance, error)
+	RegisterChatSession(ctx context.Context, authCookie state.ServerCookie) (*state.SessionInstance, error)
+	RetrieveBOSSession(ctx context.Context, authCookie state.ServerCookie) (*state.SessionInstance, error)
+	Signout(ctx context.Context, instance *state.SessionInstance)
+	SignoutChat(ctx context.Context, instance *state.SessionInstance)
 }

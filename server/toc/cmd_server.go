9dbd74276d739765838e72b39e4b903b7598fa92
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-10 10:44:20 +0000

    feat: implement userInfoToUpdateBuddy function

--- server/toc/cmd_server.go
+++ server/toc/cmd_server.go
@@ -1,6 +1,26 @@
 package toc
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pchchv/go-icq/wire"
+)
+
 var (
 	rateLimitExceededErr = "ERROR:903"
 	cmdInternalSvcErr    = "ERROR:989:internal server error"
 )
+
+// userInfoToUpdateBuddy creates an UPDATE_BUDDY server reply from a User Info TLV.
+func userInfoToUpdateBuddy(snac wire.TLVUserInfo) string {
+	online, _ := snac.Uint32BE(wire.OServiceUserInfoSignonTOD)
+	idle, _ := snac.Uint16BE(wire.OServiceUserInfoIdleTime)
+	uc := [3]string{" ", "O", " "}
+	if snac.IsAway() {
+		uc[2] = "U"
+	}
+
+	class := strings.Join(uc[:], "")
+	return fmt.Sprintf("UPDATE_BUDDY:%s:%s:%s:%d:%d:%s", snac.ScreenName, "T", fmt.Sprintf("%d", snac.WarningLevel/10), online, idle, class)
+}

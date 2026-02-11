874ad909ec102966e88f1172e5f9a87c00f5856c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-11 12:24:09 +0000

    feat(OSCARProxy): implement Profile, DirInfo and DirSearch handlers

--- server/toc/http.go
+++ server/toc/http.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"html/template"
 	"io"
 	"net"
 	"net/http"
 
+	"github.com/pchchv/go-icq/state"
 	"github.com/pchchv/go-icq/wire"
 	"golang.org/x/net/html"
 )
@@ -71,6 +73,190 @@ func (s OSCARProxy) RateLimiterMiddleware(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+// ProfileHandler handles requests to retrieve a user's profile information.
+// It queries the LocateService to fetch profile data for the specified user.
+//
+// The request must include the following query parameters:
+//   - `from`: The screen name of the user making the request.
+//   - `user`: The screen name of the user whose profile is being requested.
+//
+// If any required parameter is missing, it responds with a 400 Bad Request.
+// If the requested user is unavailable, it responds with a 404 Not Found.
+func (s OSCARProxy) ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	from := r.URL.Query().Get("from")
+	if from == "" {
+		http.Error(w, "required `from` param is missing", http.StatusBadRequest)
+		return
+	}
+
+	user := r.URL.Query().Get("user")
+	if user == "" {
+		http.Error(w, "required `user` param is missing", http.StatusBadRequest)
+		return
+	}
+
+	sess := s.SessionRetriever.RetrieveSession(state.NewIdentScreenName(from))
+	if sess == nil {
+		http.Error(w, "invalid session", http.StatusForbidden)
+		return
+	}
+
+	instances := sess.Instances()
+	if len(instances) == 0 {
+		http.Error(w, "invalid session", http.StatusForbidden)
+		return
+	}
+
+	inBody := wire.SNAC_0x02_0x05_LocateUserInfoQuery{
+		Type:       uint16(wire.LocateTypeSig),
+		ScreenName: user,
+	}
+
+	ctx := r.Context()
+	info, err := s.LocateService.UserInfoQuery(ctx, instances[0], wire.SNACFrame{}, inBody)
+	if err != nil {
+		s.logAndReturn500(ctx, w, fmt.Errorf("LocateService.UserInfoQuery: %w", err))
+		return
+	}
+
+	switch v := info.Body.(type) {
+	case wire.SNACError:
+		if v.Code == wire.ErrorCodeNotLoggedOn {
+			http.Error(w, "user is unavailable", http.StatusNotFound)
+		} else {
+			s.logAndReturn500(ctx, w, fmt.Errorf("LocateService.UserInfoQuery error code: %d", v.Code))
+		}
+	case wire.SNAC_0x02_0x06_LocateUserInfoReply:
+		profile, hasProf := v.LocateInfo.Bytes(wire.LocateTLVTagsInfoSigData)
+		if !hasProf {
+			s.logAndReturn500(ctx, w, errors.New("LocateInfo.Bytes: missing wire.LocateTLVTagsInfoSigData"))
+			return
+		}
+
+		pd := struct {
+			ScreenName string
+			Profile    template.HTML
+		}{
+			ScreenName: user,
+			Profile:    template.HTML(extractProfile(profile)),
+		}
+
+		if err := profileTemplate.Execute(w, pd); err != nil {
+			s.logAndReturn500(ctx, w, fmt.Errorf("t.Execute: %w", err))
+		}
+	default:
+		s.logAndReturn500(ctx, w, fmt.Errorf("unknown response type: %T", v))
+	}
+}
+
+// DirInfoHandler handles requests to retrieve directory information for a user.
+// It queries the LocateService to fetch directory details associated with the given screen name.
+//
+// The request must include the following query parameter:
+//   - `user`: The screen name of the user whose directory info is being requested.
+//
+// If the `user` parameter is missing, it responds with a 400 Bad Request.
+// If no directory information is found, it responds with a 404 Not Found.
+func (s OSCARProxy) DirInfoHandler(w http.ResponseWriter, request *http.Request) {
+	user := request.URL.Query().Get("user")
+	if user == "" {
+		http.Error(w, "required `user` param is missing", http.StatusBadRequest)
+		return
+	}
+
+	inBody := wire.SNAC_0x02_0x0B_LocateGetDirInfo{
+		ScreenName: user,
+	}
+	ctx := request.Context()
+	info, err := s.LocateService.DirInfo(ctx, wire.SNACFrame{}, inBody)
+	if err != nil {
+		s.logAndReturn500(ctx, w, fmt.Errorf("LocateService.DirInfo: %w", err))
+		return
+	}
+
+	switch v := info.Body.(type) {
+	case wire.SNAC_0x02_0x0C_LocateGetDirReply:
+		if len(v.TLVList) > 0 {
+			s.outputSearchResults(ctx, w, v.TLVBlock)
+		} else {
+			http.Error(w, "no user directory info found", http.StatusNotFound)
+		}
+	default:
+		s.logAndReturn500(ctx, w, fmt.Errorf("LocateService.DirInfo: unknown response type: %T", v))
+	}
+}
+
+// DirSearchHandler handles requests to perform a directory search based on various criteria.
+// It queries the DirSearchService to find users matching the specified parameters.
+// There are 3 search modes: name, email, keyword.
+//
+//   - Named-based search is toggled by the presence of either `first_name` and/or `last_name` params.
+//     The following search params can be passed:
+//     -`first_name`
+//     -`middle_name`
+//     -`last_name`
+//     -`maiden_name`
+//     -`city`
+//     -`state`
+//     -`country`
+//   - Email-based search is triggered by the`email` param.
+//   - Keyword-based search is triggered by the `keyword` param.
+//
+// If the search is missing required name parameters, it responds with a 400 Bad Request.
+func (s OSCARProxy) DirSearchHandler(w http.ResponseWriter, r *http.Request) {
+	inBody := wire.SNAC_0x0F_0x02_InfoQuery{}
+	q := r.URL.Query()
+	switch {
+	case q.Has("first_name") || q.Has("last_name"):
+		if val := q.Get("first_name"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVFirstName, val))
+		}
+		if val := q.Get("middle_name"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVMiddleName, val))
+		}
+		if val := q.Get("last_name"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVLastName, val))
+		}
+		if val := q.Get("maiden_name"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVMaidenName, val))
+		}
+		if val := q.Get("city"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVCity, val))
+		}
+		if val := q.Get("state"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVState, val))
+		}
+		if val := q.Get("country"); val != "" {
+			inBody.Append(wire.NewTLVBE(wire.ODirTLVCountry, val))
+		}
+	case q.Has("email"):
+		inBody.Append(wire.NewTLVBE(wire.ODirTLVEmailAddress, q.Get("email")))
+	case q.Has("keyword"):
+		inBody.Append(wire.NewTLVBE(wire.ODirTLVInterest, q.Get("keyword")))
+	}
+
+	ctx := r.Context()
+	info, err := s.DirSearchService.InfoQuery(ctx, wire.SNACFrame{}, inBody)
+	if err != nil {
+		s.logAndReturn500(ctx, w, fmt.Errorf("DirSearchService.InfoQuery: %w", err))
+		return
+	}
+
+	switch v := info.Body.(type) {
+	case wire.SNAC_0x0F_0x03_InfoReply:
+		switch v.Status {
+		case wire.ODirSearchResponseNameMissing:
+			http.Error(w, "missing search parameters", http.StatusBadRequest)
+		case wire.ODirSearchResponseOK:
+			s.outputSearchResults(ctx, w, v.Results.List...)
+		default:
+			s.logAndReturn500(ctx, w, fmt.Errorf("DirSearchService.InfoQuery unknown status: %d", v.Status))
+		}
+	default:
+		s.logAndReturn500(ctx, w, fmt.Errorf("DirSearchService.InfoQuery: unknown response type: %T", v))
+	}
 }
 
 func (s OSCARProxy) logAndReturn500(ctx context.Context, w http.ResponseWriter, err error) {

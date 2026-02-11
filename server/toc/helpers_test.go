98db87a4a52caca47cf30de81694d51bda123690
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-11 12:32:55 +0000

    feat: add addDenyListEntriesParams and addPermListEntriesParams types

--- server/toc/helpers_test.go
+++ server/toc/helpers_test.go
@@ -10,3 +10,15 @@ type addBuddiesParams []struct {
 	inBody wire.SNAC_0x03_0x04_BuddyAddBuddies
 	err    error
 }
+
+type addDenyListEntriesParams []struct {
+	me   state.IdentScreenName
+	body wire.SNAC_0x09_0x07_PermitDenyAddDenyListEntries
+	err  error
+}
+
+type addPermListEntriesParams []struct {
+	me   state.IdentScreenName
+	body wire.SNAC_0x09_0x05_PermitDenyAddPermListEntries
+	err  error
+}

7ba75015824777434b38816af2dd602adb8b988f
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-14 08:57:35 +0000

    feat(oscarServer): implement processFLAPAuth and processBUCPAuth methods

--- server/oscar/server.go
+++ server/oscar/server.go
@@ -1,11 +1,14 @@
 package oscar
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"log/slog"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/patrickmn/go-cache"
 	"github.com/pchchv/go-icq/config"
 	"github.com/pchchv/go-icq/state"
@@ -84,4 +87,90 @@ type oscarServer struct {
 	*IPRateLimiter
 	recalcWarning  func(ctx context.Context, instance *state.SessionInstance) error
 	lowerWarnLevel func(ctx context.Context, instance *state.SessionInstance)
+}
+
+func (s oscarServer) processFLAPAuth(ctx context.Context, signonFrame wire.FLAPSignonFrame, flapc *wire.FlapClient, advertisedHost string) error {
+	tlv, err := s.AuthService.FLAPLogin(ctx, signonFrame, advertisedHost)
+	if err != nil {
+		return err
+	}
+	return flapc.NewSignoff(tlv)
+}
+
+func (s oscarServer) processBUCPAuth(ctx context.Context, flapc *wire.FlapClient, advertisedHost string) error {
+	var frames int
+	for {
+		frame, err := flapc.ReceiveFLAP()
+		if err != nil {
+			return err
+		}
+
+		if frames > 10 {
+			// a lot of frames received, the client is misbehaving
+			return fmt.Errorf("too many auth flap packets received")
+		}
+		frames++
+
+		switch frame.FrameType {
+		case wire.FLAPFrameSignoff:
+			s.Logger.Debug("signed off mid-login")
+			return io.EOF // client disconnected
+		case wire.FLAPFrameKeepAlive:
+			s.Logger.Debug("received flap keepalive frame")
+		case wire.FLAPFrameData:
+			buf := bytes.NewReader(frame.Payload)
+			fr := wire.SNACFrame{}
+			if err := wire.UnmarshalBE(&fr, buf); err != nil {
+				return err
+			}
+
+			switch {
+			case fr.FoodGroup == wire.BUCP && fr.SubGroup == wire.BUCPChallengeRequest:
+				challengeRequest := wire.SNAC_0x17_0x06_BUCPChallengeRequest{}
+				if err := wire.UnmarshalBE(&challengeRequest, buf); err != nil {
+					return err
+				}
+
+				outSNAC, err := s.BUCPChallenge(ctx, challengeRequest, uuid.New)
+				if err != nil {
+					return err
+				}
+
+				if err := flapc.SendSNAC(outSNAC.Frame, outSNAC.Body); err != nil {
+					return err
+				}
+
+				if outSNAC.Frame.SubGroup == wire.BUCPLoginResponse {
+					screenName, _ := challengeRequest.String(wire.LoginTLVTagsScreenName)
+					s.Logger.Debug("failed BUCP challenge: user does not exist", "screen_name", screenName)
+					return nil // account does not exist
+				}
+			case fr.FoodGroup == wire.BUCP && fr.SubGroup == wire.BUCPLoginRequest:
+				loginRequest := wire.SNAC_0x17_0x02_BUCPLoginRequest{}
+				if err := wire.UnmarshalBE(&loginRequest, buf); err != nil {
+					return err
+				}
+
+				outSNAC, err := s.BUCPLogin(ctx, loginRequest, advertisedHost)
+				if err != nil {
+					return err
+				}
+
+				loginResp := outSNAC.Body.(wire.SNAC_0x17_0x03_BUCPLoginResponse)
+				// clients expect login response as SNAC on FLAP channel 2 followed by
+				// a FLAP signoff frame to properly close the auth connection
+				if err := flapc.SendSNAC(outSNAC.Frame, loginResp); err != nil {
+					return err
+				}
+
+				return flapc.NewSignoff(loginResp.TLVRestBlock)
+			default:
+				s.Logger.Debug("unexpected SNAC received during login", "foodgroup", wire.FoodGroupName(fr.FoodGroup), "subgroup", wire.SubGroupName(fr.FoodGroup, fr.SubGroup))
+				return io.EOF
+			}
+		default:
+			s.Logger.Debug("unexpected frame type received during login", "type", frame.FrameType)
+			return io.EOF
+		}
+	}
 }

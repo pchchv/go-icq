3a6159d61ea69bf133d5332da12e839ac2df818a
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-14 09:25:03 +0000

    feat: add Handler type

--- server/oscar/handler.go
+++ server/oscar/handler.go
@@ -3,6 +3,7 @@ package oscar
 import (
 	"errors"
 
+	"github.com/pchchv/go-icq/server/oscar/middleware"
 	"github.com/pchchv/go-icq/wire"
 )
 
@@ -13,4 +14,24 @@ var ErrRouteNotFound = errors.New("route not found")
 // the client from the server handlers.
 type ResponseWriter interface {
 	SendSNAC(frame wire.SNACFrame, body any) error
+}
+
+// Handler defines a structure for routing OSCAR protocol requests to
+// appropriate handlers based on group:subGroup identifiers.
+type Handler struct {
+	AdminService
+	BARTService
+	BuddyService
+	ChatNavService
+	ChatService
+	FeedbagService
+	ICBMService
+	ICQService
+	LocateService
+	ODirService
+	OServiceService
+	PermitDenyService
+	StatsService
+	UserLookupService
+	middleware.RouteLogger
 }

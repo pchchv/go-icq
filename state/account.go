064989ae93a7df493eb580f3a47d3844493ec585
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-18 12:53:14 +0000

    feat(User): implement constructor. (NewAccountCreator func)

--- state/account.go
+++ state/account.go
@@ -1,6 +1,10 @@
 package state
 
-import "context"
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
 
 // CreateAccountFunc creates a new user account in the database.
 //
@@ -12,3 +16,34 @@ import "context"
 //   - ErrDupUser: a user with the same screen name already exists
 //   - Other errors from the underlying user store (e.g., database errors)
 type CreateAccountFunc func(ctx context.Context, screenName DisplayScreenName, password string) error
+
+// NewAccountCreator returns an account creation function.
+func NewAccountCreator(insertUser func(ctx context.Context, u User) error) CreateAccountFunc {
+	return func(ctx context.Context, screenName DisplayScreenName, password string) error {
+		if screenName.IsUIN() {
+			if err := screenName.ValidateUIN(); err != nil {
+				return err
+			}
+		} else {
+			if err := screenName.ValidateAIMHandle(); err != nil {
+				return err
+			}
+		}
+
+		user := User{
+			AuthKey:           uuid.NewString(),
+			DisplayScreenName: screenName,
+			IdentScreenName:   screenName.IdentScreenName(),
+			IsICQ:             screenName.IsUIN(),
+		}
+		if err := user.HashPassword(password); err != nil {
+			return err
+		}
+
+		if err := insertUser(ctx, user); err != nil {
+			return err
+		}
+
+		return nil
+	}
+}

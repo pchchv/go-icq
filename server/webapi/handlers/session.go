7fddb8e118d984a8db4663da323fc62210689bc6
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-19 14:27:41 +0000

    refactor: remove newUserFn

--- server/webapi/handlers/session.go
+++ server/webapi/handlers/session.go
@@ -46,7 +46,7 @@ type BuddyListRegistry interface {
 // AuthService defines methods needed for authentication.
 type AuthService interface {
 	BUCPChallenge(ctx context.Context, bodyIn wire.SNAC_0x17_0x06_BUCPChallengeRequest, newUUID func() uuid.UUID) (wire.SNACMessage, error)
-	BUCPLogin(ctx context.Context, bodyIn wire.SNAC_0x17_0x02_BUCPLoginRequest, newUserFn func(screenName state.DisplayScreenName) (state.User, error), advertisedHost string) (wire.SNACMessage, error)
+	BUCPLogin(ctx context.Context, bodyIn wire.SNAC_0x17_0x02_BUCPLoginRequest, advertisedHost string) (wire.SNACMessage, error)
 	RegisterBOSSession(ctx context.Context, authCookie state.ServerCookie) (*state.SessionInstance, error)
 }
 

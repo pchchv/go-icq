dde90385f082ca487f6f5262ae1e977768ef8ce0
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-20 00:02:06 +0000

    feat: enhance config generator to process config fields and write to file

--- cmd/config_generator/main.go
+++ cmd/config_generator/main.go
@@ -1,18 +1,45 @@
+// This program generates env config scripts from config.Config struct tags for
+// unix and windows platforms.
+// Usage: go run ./cmd/config_generator [platform] [filename] [value_tag]
+// Example: go run ./cmd/config_generator unix settings.basic.env basic
 package main
 
 import (
 	"fmt"
 	"io"
 	"os"
+	"reflect"
 	"strings"
 
 	"github.com/mitchellh/go-wordwrap"
+	"github.com/pchchv/go-icq/config"
 )
+
+var platformKeywords = map[string]struct {
+	comment    string
+	assignment string
+}{
+	"windows": {
+		comment:    "rem ",
+		assignment: "set ",
+	},
+	"unix": {
+		comment:    "# ",
+		assignment: "export ",
+	},
+}
 
 func main() {
 	args := os.Args[1:]
 	if len(args) < 3 {
 		fmt.Fprintln(os.Stderr, "usage: go run cmd/config_generator [platform] [filename] [value_tag]")
+		os.Exit(1)
+	}
+
+	platform := args[0]
+	keywords, ok := platformKeywords[platform]
+	if !ok {
+		fmt.Fprintf(os.Stderr, "unable to find platform `%s`\n", platform)
 		os.Exit(1)
 	}
 
@@ -25,6 +52,31 @@ func main() {
 	}
 
 	defer f.Close()
+
+	valueTag := args[2]
+	configType := reflect.TypeOf(config.Config{})
+	for i := 0; i < configType.NumField(); i++ {
+		field := configType.Field(i)
+		// check if field is optional and has empty value
+		// use the specified value tag
+		val := field.Tag.Get(valueTag)
+		// skip optional fields with empty values
+		if field.Tag.Get("required") == "false" && val == "" {
+			continue
+		}
+
+		comment := field.Tag.Get("description")
+		if err := writeComment(f, comment, 80, keywords.comment); err != nil {
+			fmt.Fprintf(os.Stderr, "error writing to file: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		varName := field.Tag.Get("envconfig")
+		if err := writeAssignment(f, keywords.assignment, varName, val); err != nil {
+			fmt.Fprintf(os.Stderr, "error writing to file: %s\n", err.Error())
+			os.Exit(1)
+		}
+	}
 }
 
 func writeAssignment(w io.Writer, keyword string, varName string, val string) (err error) {

8cf1e13663c748f5103407705048c09faf822be7
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-21 10:37:48 +0000

    feat(StatsService): implement a ReportEvents method

--- foodgroup/stats.go
+++ foodgroup/stats.go
@@ -1,7 +1,26 @@
 package foodgroup
 
+import (
+	"context"
+
+	"github.com/pchchv/go-icq/wire"
+)
+
 type StatsService struct{}
 
 func NewStatsService() StatsService {
 	return StatsService{}
+}
+
+// ReportEvents handles incoming stats events by acknowledging them without processing.
+// This is a no-op implementation to satisfy the client's expectation of a response.
+func (s StatsService) ReportEvents(ctx context.Context, inFrame wire.SNACFrame, _ wire.SNAC_0x0B_0x03_StatsReportEvents) wire.SNACMessage {
+	return wire.SNACMessage{
+		Frame: wire.SNACFrame{
+			FoodGroup: wire.Stats,
+			SubGroup:  wire.StatsReportAck,
+			RequestID: inFrame.RequestID,
+		},
+		Body: wire.SNAC_0x0B_0x04_StatsReportAck{},
+	}
 }

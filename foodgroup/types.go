a47ac08b14a74b5d5a1cb8a8206d799b05ee9ab7
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-21 07:50:30 +0000

    feat: add ChatMessageRelayer, ChatRoomRegistry and ChatSessionRegistry types

--- foodgroup/types.go
+++ foodgroup/types.go
@@ -55,6 +55,46 @@ type BARTItemManager interface {
 	DeleteBARTItem(ctx context.Context, hash []byte) error
 }
 
+// ChatMessageRelayer defines the interface for sending messages to chat room participants.
+type ChatMessageRelayer interface {
+	// AllSessions returns all chat room participants.
+	// Returns ErrChatRoomNotFound if the room does not exist.
+	AllSessions(chatCookie string) []*state.Session
+	// RelayToAllExcept sends a message to all chat room participants except for the
+	// participant with a particular screen name.
+	// Returns ErrChatRoomNotFound if the room does not exist for cookie.
+	RelayToAllExcept(ctx context.Context, chatCookie string, except state.IdentScreenName, msg wire.SNACMessage)
+	// RelayToScreenName sends a message to a chat room user.
+	// Returns ErrChatRoomNotFound if the room does not exist for cookie.
+	RelayToScreenName(ctx context.Context, chatCookie string, recipient state.IdentScreenName, msg wire.SNACMessage)
+}
+
+// ChatRoomRegistry defines the interface for storing and retrieving chat
+// rooms in a persistent store. The persistent store has two purposes:
+//   - Remember user-created chat rooms (exchange 4) so that clients can reconnect to the rooms following server restarts.
+//   - Keep track of public chat room created by the server operator (exchange 5).
+//     User's can only join public chat rooms that exist in the room registry.
+type ChatRoomRegistry interface {
+	// ChatRoomByCookie looks up a chat room by exchange.
+	// Returns state.ErrChatRoomNotFound if the room does not exist for cookie.
+	ChatRoomByCookie(ctx context.Context, chatCookie string) (state.ChatRoom, error)
+	// ChatRoomByName looks up a chat room by exchange and name.
+	// Returns state.ErrChatRoomNotFound if the room does not exist for exchange and name.
+	ChatRoomByName(ctx context.Context, exchange uint16, name string) (state.ChatRoom, error)
+	// CreateChatRoom creates a new chat room.
+	CreateChatRoom(ctx context.Context, chatRoom *state.ChatRoom) error
+}
+
+// ChatSessionRegistry defines the interface for adding and removing chat sessions.
+type ChatSessionRegistry interface {
+	// AddSession adds a session to the chat session manager.
+	// The chatCookie param identifies the chat room to which screenName is added.
+	// It returns the newly created session instance registered in the chat session manager.
+	AddSession(ctx context.Context, chatCookie string, screenName state.DisplayScreenName) (*state.SessionInstance, error)
+	// RemoveSession removes a session from the chat session manager.
+	RemoveSession(instance *state.SessionInstance)
+}
+
 // buddyBroadcaster defines methods for broadcasting buddy presence and visibility events
 // to other sessions. These events notify users when a buddy comes online, goes offline,
 // or changes visibility status.

f7d37a1e63da57ef68c9684e40a1903a11556715
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-22 12:50:13 +0000

    feat(ODirService): implement a KeywordListQuery method

--- foodgroup/odir.go
+++ foodgroup/odir.go
@@ -1,6 +1,12 @@
 package foodgroup
 
-import "log/slog"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/pchchv/go-icq/wire"
+)
 
 // ODirService provides functionality for the ODir food group,
 // which provides functionality for searching the user directory.
@@ -15,4 +21,24 @@ func NewODirService(logger *slog.Logger, profileManager ProfileManager) ODirServ
 		logger:         logger,
 		profileManager: profileManager,
 	}
+}
+
+// KeywordListQuery returns a list of keywords that can be searched in the user directory.
+func (s ODirService) KeywordListQuery(ctx context.Context, inFrame wire.SNACFrame) (wire.SNACMessage, error) {
+	interests, err := s.profileManager.InterestList(ctx)
+	if err != nil {
+		return wire.SNACMessage{}, fmt.Errorf("InterestList: %w", err)
+	}
+
+	return wire.SNACMessage{
+		Frame: wire.SNACFrame{
+			FoodGroup: wire.ODir,
+			SubGroup:  wire.ODirKeywordListReply,
+			RequestID: inFrame.RequestID,
+		},
+		Body: wire.SNAC_0x0F_0x04_KeywordListReply{
+			Status:    0x01,
+			Interests: interests,
+		},
+	}, nil
 }

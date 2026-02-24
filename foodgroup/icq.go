db484bc10d75a90f098daff52f5ddf069384b6f7
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-24 11:05:15 +0000

    feat(ICQService): implement a reply and a reqAck methods

--- foodgroup/icq.go
+++ foodgroup/icq.go
@@ -1,8 +1,12 @@
 package foodgroup
 
 import (
+	"context"
 	"log/slog"
 	"time"
+
+	"github.com/pchchv/go-icq/state"
+	"github.com/pchchv/go-icq/wire"
 )
 
 // ICQService provides functionality for the ICQ food group.
@@ -34,4 +38,35 @@ func NewICQService(
 		offlineMessageManager: offlineMessageManager,
 		timeNow:               time.Now,
 	}
+}
+
+func (s ICQService) reply(ctx context.Context, instance *state.SessionInstance, message wire.ICQMessageReplyEnvelope) error {
+	s.messageRelayer.RelayToScreenName(ctx, instance.IdentScreenName(), wire.SNACMessage{
+		Frame: wire.SNACFrame{
+			FoodGroup: wire.ICQ,
+			SubGroup:  wire.ICQDBReply,
+		},
+		Body: wire.SNAC_0x15_0x02_DBReply{
+			TLVRestBlock: wire.TLVRestBlock{
+				TLVList: wire.TLVList{
+					wire.NewTLVBE(wire.ICQTLVTagsMetadata, message),
+				},
+			},
+		},
+	})
+	return nil
+}
+
+func (s ICQService) reqAck(ctx context.Context, instance *state.SessionInstance, seq uint16, subType uint16) error {
+	return s.reply(ctx, instance, wire.ICQMessageReplyEnvelope{
+		Message: wire.ICQ_0x07DA_0x00DC_DBQueryMetaReplyMoreInfo{
+			ICQMetadata: wire.ICQMetadata{
+				UIN:     instance.UIN(),
+				ReqType: wire.ICQDBQueryMetaReply,
+				Seq:     seq,
+			},
+			ReqSubType: subType,
+			Success:    wire.ICQStatusCodeOK,
+		},
+	})
 }

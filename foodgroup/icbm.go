0f37df84bbcca7a4f917d42a80273ac13d151105
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-25 10:30:32 +0000

    feat: add a convoTracker type and implement its constructor

---
+++ foodgroup/icbm.go
@@ -0,0 +1,27 @@
+package foodgroup
+
+import (
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+// convoTracker keeps track of messages initiated from a sender to a recipient.
+// A user (the warner) can only warn another user (the warnee)
+// only if the warner has received a message from the warnee.
+// The warner may only warn 1 time per message received from warnee.
+// The warner may only warn the warnee up to 3 times per warn window.
+type convoTracker struct {
+	convos *cache.Cache
+	warns  *cache.Cache
+	window time.Duration
+}
+
+func newConvoTracker() *convoTracker {
+	window := 1 * time.Hour
+	return &convoTracker{
+		convos: cache.New(window, window),
+		warns:  cache.New(window, window),
+		window: window,
+	}
+}

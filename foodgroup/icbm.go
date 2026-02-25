a0f8704624ed67c6cef9b884214f676e008278b4
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-25 10:33:50 +0000

    feat(ringBuffer): implement set and val methods

--- foodgroup/icbm.go
+++ foodgroup/icbm.go
@@ -13,6 +13,17 @@ type ringBuffer struct {
 	vals [3]time.Time // Fixed-size array to store time values.
 }
 
+// set stores the given time at the current cursor position and advances the cursor.
+func (r *ringBuffer) set(v time.Time) {
+	r.vals[r.cur] = v
+	r.cur = (r.cur + 1) % len(r.vals)
+}
+
+// val returns the time at the current cursor position.
+func (r *ringBuffer) val() time.Time {
+	return r.vals[r.cur]
+}
+
 // convoTracker keeps track of messages initiated from a sender to a recipient.
 // A user (the warner) can only warn another user (the warnee)
 // only if the warner has received a message from the warnee.

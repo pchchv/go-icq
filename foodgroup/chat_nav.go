0cf2d7344313451629320a06d624ab53ff502aea
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-26 10:27:47 +0000

    feat(ChatNavService): implement RequestRoomInfo and RequestChatRights methods

--- foodgroup/chat_nav.go
+++ foodgroup/chat_nav.go
@@ -11,10 +11,11 @@ import (
 )
 
 var (
-	errChatNavRetrieveFailed   = errors.New("unable to retrieve chat room chat room")
-	errChatNavRoomNameMissing  = errors.New("unable to find chat name in TLV payload")
-	errChatNavRoomCreateFailed = errors.New("unable to create chat room")
-	defaultExchangeCfg         = wire.TLVBlock{
+	errChatNavMismatchedExchange = errors.New("chat room exchange does not match requested exchange")
+	errChatNavRoomNameMissing    = errors.New("unable to find chat name in TLV payload")
+	errChatNavRetrieveFailed     = errors.New("unable to retrieve chat room chat room")
+	errChatNavRoomCreateFailed   = errors.New("unable to create chat room")
+	defaultExchangeCfg           = wire.TLVBlock{
 		TLVList: wire.TLVList{
 			wire.NewTLVBE(wire.ChatRoomTLVMaxConcurrentRooms, uint8(10)),
 			wire.NewTLVBE(wire.ChatRoomTLVClassPerms, uint16(0x0010)),
@@ -124,6 +125,71 @@ func (s ChatNavService) ExchangeInfo(_ context.Context, inFrame wire.SNACFrame, 
 			},
 		},
 	}, nil
+}
+
+// RequestRoomInfo returns wire.ChatNavNavInfo,
+// which contains metadata for the chat room specified in the inFrame.hmacCookie.
+func (s ChatNavService) RequestRoomInfo(ctx context.Context, inFrame wire.SNACFrame, inBody wire.SNAC_0x0D_0x04_ChatNavRequestRoomInfo) (wire.SNACMessage, error) {
+	if err := validateExchange(inBody.Exchange); err != nil {
+		s.logger.Debug("error validating exchange: " + err.Error())
+		return sendChatNavErrorSNAC(inFrame, wire.ErrorCodeNotSupportedByHost)
+	}
+
+	room, err := s.chatRoomManager.ChatRoomByCookie(ctx, inBody.Cookie)
+	if err != nil {
+		return wire.SNACMessage{}, fmt.Errorf("%w: %w", state.ErrChatRoomNotFound, err)
+	} else if room.Exchange() != inBody.Exchange {
+		return wire.SNACMessage{}, errChatNavMismatchedExchange
+	}
+
+	return wire.SNACMessage{
+		Frame: wire.SNACFrame{
+			FoodGroup: wire.ChatNav,
+			SubGroup:  wire.ChatNavNavInfo,
+			RequestID: inFrame.RequestID,
+		},
+		Body: wire.SNAC_0x0D_0x09_ChatNavNavInfo{
+			TLVRestBlock: wire.TLVRestBlock{
+				TLVList: wire.TLVList{
+					wire.NewTLVBE(wire.ChatNavTLVRoomInfo, wire.SNAC_0x0E_0x02_ChatRoomInfoUpdate{
+						Cookie:         room.Cookie(),
+						Exchange:       room.Exchange(),
+						DetailLevel:    room.DetailLevel(),
+						InstanceNumber: room.InstanceNumber(),
+						TLVBlock: wire.TLVBlock{
+							TLVList: room.TLVList(),
+						},
+					}),
+				},
+			},
+		},
+	}, nil
+}
+
+// RequestChatRights returns SNAC wire.ChatNavNavInfo, which contains chat navigation service parameters and limits.
+func (s ChatNavService) RequestChatRights(_ context.Context, inFrame wire.SNACFrame) wire.SNACMessage {
+	return wire.SNACMessage{
+		Frame: wire.SNACFrame{
+			FoodGroup: wire.ChatNav,
+			SubGroup:  wire.ChatNavNavInfo,
+			RequestID: inFrame.RequestID,
+		},
+		Body: wire.SNAC_0x0D_0x09_ChatNavNavInfo{
+			TLVRestBlock: wire.TLVRestBlock{
+				TLVList: wire.TLVList{
+					wire.NewTLVBE(wire.ChatNavTLVMaxConcurrentRooms, uint8(10)),
+					wire.NewTLVBE(wire.ChatNavTLVExchangeInfo, wire.SNAC_0x0D_0x09_TLVExchangeInfo{
+						Identifier: state.PrivateExchange,
+						TLVBlock:   defaultExchangeCfg,
+					}),
+					wire.NewTLVBE(wire.ChatNavTLVExchangeInfo, wire.SNAC_0x0D_0x09_TLVExchangeInfo{
+						Identifier: state.PublicExchange,
+						TLVBlock:   defaultExchangeCfg,
+					}),
+				},
+			},
+		},
+	}
 }
 
 // sendChatNavErrorSNAC returns a ChatNavErr SNAC and logs an error for the operator.

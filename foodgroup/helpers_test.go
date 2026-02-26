21a27d20547b217d59415272c47206d58d959471
Evgeni Pochechuev <ipchchv@gmail.com>
2026-02-26 11:07:42 +0000

    feat(tests): add parameter types for mock AddBuddy and AddSession calls

--- foodgroup/helpers_test.go
+++ foodgroup/helpers_test.go
@@ -6,24 +6,24 @@ import (
 	"github.com/pchchv/go-icq/state"
 )
 
-// accountManagerConfirmStatusParams is the list of parameters passed at the mock
-// accountManager.ConfirmStatus call site
+// accountManagerConfirmStatusParams is the list of parameters passed at
+// the mock accountManager.ConfirmStatus call site.
 type accountManagerConfirmStatusParams []struct {
 	screenName    state.IdentScreenName
 	confirmStatus bool
 	err           error
 }
 
-// accountManagerEmailAddressParams is the list of parameters passed at the mock
-// accountManager.EmailAddress call site
+// accountManagerEmailAddressParams is the list of parameters passed at
+// the mock accountManager.EmailAddress call site.
 type accountManagerEmailAddressParams []struct {
 	screenName   state.IdentScreenName
 	emailAddress *mail.Address
 	err          error
 }
 
-// accountManagerParams is a helper struct that contains mock parameters for
-// accountManager methods
+// accountManagerParams is a helper struct that
+// contains mock parameters for accountManager methods.
 type accountManagerParams struct {
 	accountManagerUpdateDisplayScreenNameParams
 	accountManagerUpdateEmailAddressParams
@@ -36,57 +36,74 @@ type accountManagerParams struct {
 	accountManagerSetUserPasswordParams
 }
 
-// accountManagerRegStatusParams is the list of parameters passed at the mock
-// accountManager.RegStatus call site
+// accountManagerRegStatusParams is the list of parameters passed at
+// the mock accountManager.RegStatus call site.
 type accountManagerRegStatusParams []struct {
 	screenName state.IdentScreenName
 	regStatus  uint16
 	err        error
 }
 
-// accountManagerSetUserPasswordParams is the list of parameters passed at the mock
-// accountManager.SetUserPassword call site
+// accountManagerSetUserPasswordParams is the list of parameters passed at
+// the mock accountManager.SetUserPassword call site.
 type accountManagerSetUserPasswordParams []struct {
 	screenName state.IdentScreenName
 	password   string
 	err        error
 }
 
-// accountManagerUpdateConfirmStatusParams is the list of parameters passed at the mock
-// accountManager.UpdateConfirmStatus call site
+// accountManagerUpdateConfirmStatusParams is the list of parameters passed at
+// the mock accountManager.UpdateConfirmStatus call site.
 type accountManagerUpdateConfirmStatusParams []struct {
 	confirmStatus bool
 	screenName    state.IdentScreenName
 	err           error
 }
 
-// accountManagerUpdateDisplayScreenNameParams is the list of parameters passed at the mock
-// accountManager.UpdateDisplayScreenName call site
+// accountManagerUpdateDisplayScreenNameParams is the list of parameters passed at
+// the mock accountManager.UpdateDisplayScreenName call site.
 type accountManagerUpdateDisplayScreenNameParams []struct {
 	displayScreenName state.DisplayScreenName
 	err               error
 }
 
-// accountManagerUpdateEmailAddressParams is the list of parameters passed at the mock
-// accountManager.UpdateEmailAddress call site
+// accountManagerUpdateEmailAddressParams is the list of parameters passed at
+// the mock accountManager.UpdateEmailAddress call site.
 type accountManagerUpdateEmailAddressParams []struct {
 	emailAddress *mail.Address
 	screenName   state.IdentScreenName
 	err          error
 }
 
-// accountManagerUpdateRegStatusParams is the list of parameters passed at the mock
-// accountManager.UpdateRegStatus call site
+// accountManagerUpdateRegStatusParams is the list of parameters passed at
+// the mock accountManager.UpdateRegStatus call site.
 type accountManagerUpdateRegStatusParams []struct {
 	regStatus  uint16
 	screenName state.IdentScreenName
 	err        error
 }
 
-// accountManagerUserParams is the list of parameters passed at the mock
-// accountManager.User call site
+// accountManagerUserParams is the list of parameters passed at
+// the mock accountManager.User call site.
 type accountManagerUserParams []struct {
 	screenName state.IdentScreenName
 	result     *state.User
 	err        error
+}
+
+// legacyBuddiesParams is the list of parameters passed at
+// the mock ClientSideBuddyListManager.AddBuddy call site.
+type addBuddyParams []struct {
+	me   state.IdentScreenName
+	them state.IdentScreenName
+	err  error
+}
+
+// addSessionParams is the list of parameters passed at
+// the mock SessionRegistry.AddSession call site.
+type addSessionParams []struct {
+	screenName  state.DisplayScreenName
+	doMultiSess bool
+	result      *state.SessionInstance
+	err         error
 }
